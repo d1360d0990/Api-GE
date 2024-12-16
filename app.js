9b49a429dec29da942c40c476143fe6f23541389
Diego Orlando Diaz <diego.d0990@gmail.com>
2024-12-16 17:19:50 -0300

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const dbconnect = require('./config/db');
 const eventosRoutes = require('./routes/eventRoute');
-const port = 4000
+const port = process.env.PORT || 4000
 const cors = require ('cors')
 
 //----------------------------------VerCel - Documentations -------------------------------------------------
