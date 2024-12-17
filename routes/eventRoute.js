90e9714dc33366b56f378c9b1f51f1e138854b7a
Diego Orlando Diaz <diego.d0990@gmail.com>
2024-12-16 21:54:33 -0300

    .

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -7,9 +7,9 @@ const modelEvent = require('../models/eventModel'); // Importar el modelo de lib
 router.get('/events', async (req, res) => {
     try {
         const eventos = await modelEvent.find(); // Obtener todos los libros
-        res.status(200).send(eventos);
+        res.status(200).json(eventos);
     } catch (error) {
-        res.status(500).send({ mensaje: 'Error al obtener los eventos', error });
+        res.status(500).json({ mensaje: 'Error al obtener los eventos', error });
     }
 });
 
@@ -18,11 +18,11 @@ router.get('/eventos/:id', async (req, res) => {
     try {
         const libro = await modelEvento.findById(req.params.id); // Buscar libro por ID
         if (!libro) {
-            return res.status(404).send({ mensaje: 'Libro no encontrado' });
+            return res.status(404).json({ mensaje: 'Libro no encontrado' });
         }
-        res.status(200).send(eventos);
+        res.status(200).json(eventos);
     } catch (error) {
-        res.status(500).send({ mensaje: 'Error al obtener el evento', error });
+        res.status(500).json({ mensaje: 'Error al obtener el evento', error });
     }
 });
 
@@ -31,9 +31,9 @@ router.post('/addEvent', async (req, res) => {
     const body = req.body;
     try {
         const nuevoEvento = await modelEvent.create(body); // Insertar en la base de datos
-        res.status(201).send(nuevoEvento); // 201 indica que se ha creado un recurso
+        res.status(201).json(nuevoEvento); // 201 indica que se ha creado un recurso
     } catch (error) {
-        res.status(400).send(error); // Manejar errores
+        res.status(400).json(error); // Manejar errores
     }
 });
 
@@ -42,11 +42,11 @@ router.put('/eventos/:id', async (req, res) => {
     try {
         const eventoActualizado = await modelEvent.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
         if (!eventoActualizado) {
-            return res.status(404).send({ mensaje: 'Evento no encontrado' });
+            return res.status(404).json({ mensaje: 'Evento no encontrado' });
         }
-        res.status(200).send(eventoActualizado);
+        res.status(200).json(eventoActualizado);
     } catch (error) {
-        res.status(400).send({ mensaje: 'Error al actualizar el evento', error });
+        res.status(400).json({ mensaje: 'Error al actualizar el evento', error });
     }
 });
 
@@ -55,11 +55,11 @@ router.delete('/eventos/:id', async (req, res) => {
     try {
         const eventoEliminado = await modelEvent.findByIdAndDelete(req.params.id); // Eliminar libro por ID
         if (!eventoEliminado) {
-            return res.status(404).send({ mensaje: 'Evento no encontrado' });
+            return res.status(404).json({ mensaje: 'Evento no encontrado' });
         }
-        res.status(200).send({ mensaje: 'Evento eliminado correctamente' });
+        res.status(200).json({ mensaje: 'Evento eliminado correctamente' });
     } catch (error) {
-        res.status(500).send({ mensaje: 'Error al eliminar el evento', error });
+        res.status(500).json({ mensaje: 'Error al eliminar el evento', error });
     }
 });
 
@@ -77,13 +77,13 @@ router.get ('/eventos/negocio/busqueda', async (req,res)=>{
         const evento = await modelEvent.find (query);
 
         if(!evento.length){
-            return res.status (404).send({mensaje: 'No se encontraron eventos con los fliltros proporcionados'});
+            return res.status (404).json({mensaje: 'No se encontraron eventos con los fliltros proporcionados'});
         }
-        res.status (200).send(eventos);
+        res.status (200).json(eventos);
 
 
     }catch (error){
-        res.status (500).send ({mensaje: 'Error al obtener los eventos', error})
+        res.status (500).json ({mensaje: 'Error al obtener los eventos', error})
     }
 })
 
