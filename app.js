8c30f1f0591082f1020f6b5957ae7a1016680bb6
Diego Orlando Diaz <diego.d0990@gmail.com>
2024-12-16 22:14:26 -0300

    .

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 //----------------------------------VerCel - Documentations -------------------------------------------------
 
-//app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 
 
